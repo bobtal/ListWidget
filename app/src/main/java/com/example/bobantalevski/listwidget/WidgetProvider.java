ef04dba336df74c84c8593475caf4ae25acb4f8f
bobtal <talevski.boban@gmail.com>
2018-01-07 15:08:54 +0100

    end of Making it Change Color video

--- app/src/main/java/com/example/bobantalevski/listwidget/WidgetProvider.java
+++ app/src/main/java/com/example/bobantalevski/listwidget/WidgetProvider.java
@@ -3,6 +3,7 @@ package com.example.bobantalevski.listwidget;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
@@ -11,12 +12,22 @@ public class WidgetProvider extends AppWidgetProvider{
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        for (int id : appWidgetIds) {
+        int[] realAppWidgetIds = AppWidgetManager.getInstance(context).
+                getAppWidgetIds(new ComponentName(context, WidgetProvider.class));
+
+        for (int id : realAppWidgetIds) {
             RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget);
+
+            // get a random color
+            int r = (int)(Math.random() * 0xff);
+            int g = (int)(Math.random() * 0xff);
+            int b = (int)(Math.random() * 0xff);
+            int color = (0xff << 24) + (r << 16) + (g << 8) + b;
+            remoteViews.setInt(R.id.frameLayout, "setBackgroundColor", color);
 
             Intent intent = new Intent(context, WidgetProvider.class);
             intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
-            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, realAppWidgetIds);
 
             PendingIntent pendingIntent = PendingIntent.getBroadcast(
                     context,
