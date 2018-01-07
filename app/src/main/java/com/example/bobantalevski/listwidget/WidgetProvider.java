ca8ed0529fdaa36db4f6572ef2f949f7a76ebed2
bobtal <talevski.boban@gmail.com>
2018-01-07 14:32:46 +0100

    end of Clicking the Widget video

--- app/src/main/java/com/example/bobantalevski/listwidget/WidgetProvider.java
+++ app/src/main/java/com/example/bobantalevski/listwidget/WidgetProvider.java
@@ -1,8 +1,10 @@
 package com.example.bobantalevski.listwidget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
 
 public class WidgetProvider extends AppWidgetProvider{
@@ -11,6 +13,19 @@ public class WidgetProvider extends AppWidgetProvider{
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         for (int id : appWidgetIds) {
             RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.widget);
+
+            Intent intent = new Intent(context, WidgetProvider.class);
+            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetIds);
+
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(
+                    context,
+                    0,
+                    intent,
+                    PendingIntent.FLAG_UPDATE_CURRENT
+            );
+            remoteViews.setOnClickPendingIntent(R.id.frameLayout, pendingIntent);
+
             appWidgetManager.updateAppWidget(id, remoteViews);
         }
     }
